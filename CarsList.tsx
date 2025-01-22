45f4ff9b2b9d481d6f5d4a805407899e2f96ef3d
Aaron <107720212+aaronab3r@users.noreply.github.com>
2025-01-21 20:13:14 -0500

    Add files via upload

--- CarsList.tsx
+++ CarsList.tsx
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import CarCard from "./CarCard";
+import BookingModal from "../CarBooking/BookingModal";
 
-function CarsList(props: any) {
+function CarsList({
+  carsList,
+  isLoaded,
+  initialLocations = [], // Optional locations prop
+}: {
+  carsList: any[];
+  isLoaded: boolean;
+  initialLocations?: any[]; // Optional
+}) {
+  const [selectedCar, setSelectedCar] = useState<any>(null);
+
+  const openModal = (car: any) => {
+    setSelectedCar(car);
+    const modal = document.getElementById("my_modal_4") as HTMLDialogElement;
+    if (modal) {
+      modal.showModal();
+    }
+  };
+
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {props.carsList.map((car: any, index: number) => (
-        <div key={car.id || index}>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {/* Render Car Cards */}
+      {carsList.map((car: any, index: number) => (
+        <div key={car.id || index} onClick={() => openModal(car)}>
           <CarCard car={car} />
         </div>
       ))}
+
+      {/* Modal */}
+      <dialog id="my_modal_4" className="modal">
+        {selectedCar && <BookingModal car={selectedCar} initialLocations={initialLocations} />}
+      </dialog>
     </div>
   );
 }
 
 export default CarsList;
-
-/* 
-import React from 'react'
-import CarCard from './CarCard'
-
-function CarsList(props:any) {
-  return (
-    <div>
-        {props.carsList.map((car:any,index:number)=>(
-            <div>
-                <CarCard car={car} />
-            </div>
-        ))}
-    </div>
-  )
-}
-
-export default CarsList 
-*/
