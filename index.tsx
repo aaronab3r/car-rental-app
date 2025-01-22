7203630fc7a970ab2328f35f10eea4441ba04dae
Aaron <107720212+aaronab3r@users.noreply.github.com>
2025-01-22 00:15:47 -0500

    updated

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import request, { gql } from "graphql-request";
-
+const MASTER_URL = "https://us-west-2.cdn.hygraph.com/content/cm5o6yjf902r007w13joi3lfn/master"
 export const getCarsList=async()=> {
   const query = gql`
     query MyQuery {
@@ -15,13 +15,47 @@ export const getCarsList=async()=> {
         image {
             url
         }
+        carType
+        carBrand
       }
     }
-  `;
-
-  const result = await request(
-    "https://us-west-2.cdn.hygraph.com/content/cm5o6yjf902r007w13joi3lfn/master",
-    query
-  );
+  ;
+`
+  const result=await request(MASTER_URL,query);
   return result;
 };
+
+
+export const getStoreLocations=async()=>{
+  const query=gql`
+  query storeLocation {
+  storesLocations {
+    address
+  }
+}
+`
+
+  const result=await request(MASTER_URL,query);
+  return result;
+}
+
+export const createBooking=async(formValue:any)=>{
+  const mutationQuery=gql`
+  mutation MyMutation {
+  createBooking(
+    data: {userName: "", 
+      pickUpDate: "", 
+      pickUpTime: "", 
+      dropOffDate: "", 
+      dropOffTime: "", 
+      contactNumber: "", 
+      carId: {connect: {id: ""}}}
+  ) {
+    id
+  }
+}
+` 
+  
+  const result=await request(MASTER_URL,mutationQuery);
+  return result;
+}
