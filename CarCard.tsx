a1fc164fd46b7a56fec8add5b1e92895b2a1bf27
Aaron <107720212+aaronab3r@users.noreply.github.com>
2025-01-21 20:12:17 -0500

    Add files via upload

--- CarCard.tsx
+++ CarCard.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaGasPump } from "react-icons/fa";
 import { MdAirlineSeatReclineNormal } from "react-icons/md";
 import { PiSteeringWheelFill } from "react-icons/pi";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 function CarCard(props: any) {
-  const [car, setCar] = useState(props.car);
+  const [car, setCar] = useState<any>();
 
-  return (
+  useEffect(()=>{
+    if(props.car)
+    {
+      setCar(props.car)
+    }
+  },[props.car])
+
+  return car&&(
     <div className="group bg-gray-50 p-4 hover:bg-white hover:border-[1px] cursor-pointer duration-300 border-blue-500 relative">
       {/* Car Details */}
-      <h2 className="text-[25px] font-medium mb-2">{car.name}</h2>
+      <h2 className="text-[25px] font-medium mb-2">{car?.name}</h2>
       <span className="text-[22px] font-medium">${car.price}</span>
       <span className="text-[18px] font-light"> /day</span>
 
